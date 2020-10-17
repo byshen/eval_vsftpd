87ab87b7194288a09a4bb12d1973367663d04cd4
Bingyu Shen <ahshenbingyu@gmail.com>
2020-10-17 14:53:52 -0700

    fix bug

--- privops.c
+++ privops.c
@@ -175,7 +175,7 @@ vsf_privop_pasv_listen(struct vsf_session* p_sess)
       struct mystr tmp_log;
       str_alloc_text(&tmp_log, "Could not bind to port: ");
       str_append_ulong(&tmp_log, the_port);
-      str_append_str(&tmp_log, " chosen based on configuration: pasv_max_port and pasv_min_port");
+      str_append_text(&tmp_log, " chosen based on configuration: pasv_max_port and pasv_min_port");
       vsf_log_line_fail(p_sess, kVSFLogEntryConnection, &tmp_log);;
     }
     /* SELinux systems can give you an inopportune EACCES, it seems. */
